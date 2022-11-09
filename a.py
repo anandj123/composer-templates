113d78de618444a999b7093e50b65e5b3e5d044b
Anand Jain <jain.12@gmail.com>
2022-11-09 15:07:02 -0500

    test

--- a.py
+++ a.py
@@ -1,14 +1,7 @@
-import pytest
-
 
 def test_sum(x,y):
     return x+y
 
-def unit_test():
-    assert test_sum(3,4) == 7
-
-def unit_test2():
-    assert test_sum(2,4) == 7
 
 if __name__ == "__main__":
     test_sum(3,5)
--- a_test.py
+++ a_test.py
@@ -1,5 +1,4 @@
 # test_with_unittest.py
-import pytest
 from unittest import TestCase
 from xml.etree.ElementInclude import include
 import a
@@ -10,6 +9,7 @@ class TryTesting(TestCase):
     def test_always_passes(self):
         assert a.test_sum(3,4) == 7
      
-        
+    def unit_test2(self):
+        assert a.test_sum(2,4) == 7    
 
     
