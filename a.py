f212e5d2bc15bdd06ecfdeee0441c25bf24694e2
Anand Jain <jain.12@gmail.com>
2022-11-09 15:04:46 -0500

    test

--- a.py
+++ a.py
@@ -6,7 +6,10 @@ def test_sum(x,y):
 
 def unit_test():
     assert test_sum(3,4) == 7
-    
+
+def unit_test2():
+    assert test_sum(2,4) == 7
+
 if __name__ == "__main__":
     test_sum(3,5)
     print("Everything passed")
