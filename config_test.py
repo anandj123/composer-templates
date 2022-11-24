e9ecd6e4a72eb8fd12f93225a7cf9f74299730c3
Anand Jain <jain.12@gmail.com>
2022-11-24 14:33:38 -0500

    test

--- config_test.py
+++ config_test.py
@@ -6,3 +6,5 @@ def test_tasks():
     assert(len(config_data['tasks'])>0)
 def test_dag_name():
     assert(len(config_data['dag_name'])>0)
+def test_scheduled_interval():
+    assert(len(config_data['schedule_interval'])>0)
