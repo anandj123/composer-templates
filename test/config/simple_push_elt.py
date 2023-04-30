7b5577922d9e2bec892e27f43c56eb37bade1cc4
Anand Jain <jain.12@gmail.com>
2023-04-30 16:19:05 -0400

    simple dag fix

--- test/config/simple_push_elt.py
+++ test/config/simple_push_elt.py
@@ -2,30 +2,29 @@ import airflow
 from datetime import datetime
 from airflow.models import DAG
 from airflow.operators.dummy_operator import DummyOperator
-from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
-from airflow.providers.google.cloud.operators.bigquery import BigQueryInsertJobOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.bash_operator import BashOperator
 
 dag = DAG(
     dag_id='simple_push_elt',
-    schedule_interval='@hourly',
-    start_date=airflow.utils.dates.days_ago(0),
-    catchup=False,
+    schedule_interval=None,
+    start_date=airflow.utils.dates.days_ago(0)
 )
- 
-with dag:
-    start = DummyOperator(
-        task_id='start',
-        dag=dag
-    )
-    bash1 = BashOperator(
-         task_id='bash1',
-         bash_command='echo "hello 1"')
-    bash2 = BashOperator(
-         task_id='bash2',
-         bash_command='echo "hello 2"')
-    bash3 = BashOperator(
-         task_id='bash3',
-         bash_command='echo "hello 3"')
 
-    start >> bash1 >> bash2 >> bash3
+with dag:
+    start = DummyOperator( task_id='start')
+    bash_task_1 = airflow.operators.bash_operator.BashOperator (
+                        task_id = 'bash_task_1',
+                        bash_command = 'echo "hello 1"',
+                        trigger_rule='none_failed')
+    bash_task_2 = airflow.operators.bash_operator.BashOperator (
+                        task_id = 'bash_task_2',
+                        bash_command = 'echo "hello 2"',
+                        trigger_rule='none_failed')
+    bash_task_3 = airflow.operators.bash_operator.BashOperator (
+                        task_id = 'bash_task_3',
+                        bash_command = 'echo "hello 3"',
+                        trigger_rule='none_failed')
+
+    start >> bash_task_1 >> bash_task_2 >> bash_task_3
